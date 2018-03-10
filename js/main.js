1d97767c99aa670c79326c4fe9afea63ab504d5a
bernie <bjorn.hedelund@insoft.se>
2018-03-10 23:17:34 +0100

    Super Mario revamp

--- js/main.js
+++ js/main.js
@@ -4,21 +4,18 @@
         checksList = doc.getElementById("password-checks");
     var passwordplain;
 
-    // Code to render the time returned by HSIMP
-    var renderTime = function (time, input) {
-        timeDiv.innerHTML = time || "";
-    };
-
     // Code to output the checks returned by HSIMP
     var renderChecks = function (checks, input) {
         checksList.innerHTML = "";
 
         for (var i = 0, l = checks.length; i < l; i++) {
             var li = doc.createElement("li"),
-                title = doc.createElement("h2"),
+                title = doc.createElement("h3"),
                 message = doc.createElement("p");
 
+            title.className = "c64pm px16";
             title.innerHTML = checks[i].name;
+            li.className = "c64pm px16";
             li.appendChild(title);
 
             message.innerHTML = checks[i].message;
@@ -35,7 +32,6 @@
             good: 31557600e9, // 1 billion years
             ok: 31557600e3 // 1 thousand years
         },
-        outputTime: renderTime,
         outputChecks: renderChecks
     });
 
@@ -69,13 +65,13 @@ window.setInterval(function(){
         if (this.readyState == 4 && this.status == 200) {
           var xhttpresponse = this.responseText;
           if (xhttpresponse.indexOf(subsha1pass) !== -1) {
-            document.getElementById("iscompromised").innerHTML = '<span style="color: #ff0000;">Oh no! This password was found in a database of compromised passwords! If this is your password, you should change it immediately. Using a password that has been breached is extremely dangerous. <h4>If you are using this password on multiple websites, you should take the opportunity to start using different passwords for every website. Attackers can take the advantage of password reuse by automating login attempts on your account using breached emails and password pairs.</h4></span>';
+            document.getElementById("iscompromised").innerHTML = '<img src="img/mario_fail.gif"><br><span style="color: #ff0000;" class="c64pm px16">Oh no! This password was found in the database for stolen credentials.</h4></span><br><span style="color: #ff0000;" class="c64pm px16">Having a stolen (publicly known) password is dangerous. Change it without delay.</h4></span>';
           }else {
-            document.getElementById("iscompromised").innerHTML = '<span style="color: #339966;">Good news, this password has never been breached!</span>';
+            document.getElementById("iscompromised").innerHTML = '<span style="color: #339966;" class="c64pm px16"><img src="img/mario_success.gif"><br>Good news, this password has never been breached!</span>';
           }
         }else {
           if (this.status == 404) {
-            document.getElementById("iscompromised").innerHTML = '<span style="color: #339966;">Good news, this password has never been breached!</span>';
+            document.getElementById("iscompromised").innerHTML = '<span style="color: #339966;" class="c64pm px16">Good news, this password has never been breached!</span>';
           }
         }
       };
@@ -91,7 +87,7 @@ window.setInterval(function(){
 function passwordmodified() {
   var modifiedpassword = passwordInput.value;
   if (modifiedpassword !== passwordplain) {
-    document.getElementById("iscompromised").innerHTML = '<span style="color: #ff9900;"><img src="img/loading.gif" alt="" width="25" height="25" />&nbsp;We are checking if your password has ever been compromised...</span>';
+    document.getElementById("iscompromised").innerHTML = '<span style="color: #ff9900;" class="c64pm px16"><img src="img/loading.gif" alt="" width="25" height="25" />&nbsp;We are checking if your password has ever been compromised...</span>';
   }
 }
 
@@ -234,3 +230,5 @@ function SHA1 (msg) {
     var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
     return temp.toLowerCase();
 }
+
+
